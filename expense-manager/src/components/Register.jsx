f556b5073a9717e169d02d1191bf1d702401bd16
Gouthami Kyatham <gouthamikyatham4@gmail.com>
2020-10-10 15:40:50 +0530

    components

---
+++ expense-manager/src/components/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Register() {
+    return (
+        <div>
+            <label>Name</label>
+            <input type="text"/>
+        </div>
+    )
+}
